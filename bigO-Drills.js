b535465ae32112fcc24c15ca8b9cf5b215ff845b
David <dajohnson1998@gmail.com>
2019-02-26 11:55:21 -0500

    Finished GenerateFib Drill

--- bigO-Drills.js
+++ bigO-Drills.js
@@ -63,3 +63,35 @@ function createPairs(arr) {
 
 /* This function has a Big-O of O(n^2) because it has to loop over the provided array twice every time
 the function is called */
+
+
+function generateFib(num) {
+  let result = [];
+  for (let i = 1; i <= num; i++) {
+
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i == 2) {
+      result.push(1);
+    }
+
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+
+/*
+If num is 0, or 1 the function Big-O of O(1)
+In any other case, This function has a Big-O of O(n)
+*/
