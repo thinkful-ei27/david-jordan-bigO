5492ebda6fc99e2f1d3efcb6a7dcd35c612e3070
Jordan Heffernan <jheff2@gmail.com>
2019-02-26 09:09:01 -0800

    prime drill finished

--- bigO-Drills.js
+++ bigO-Drills.js
@@ -131,3 +131,23 @@ function findRandomElement(arr) {
 /*
 This function is O(1), because it will return the first random array index value
 */
+
+
+function isPrime(n) {
+  // if n is less than 2 or a decimal, it's not prime
+  if (n < 2 || n % 1 != 0) {
+    return false;
+  }
+  // otherwise, check if `n` is divisible by any integer
+  // between 2 and n.
+  for (let i = 2; i < n; ++i) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+/*
+Best case will be O(1) if the number is two or less the function will run once and return false.
+Otherwise it is O(n). in worst case scenarios i will have to increment up to the value of n in order to find
+prime numbers.
+*/
